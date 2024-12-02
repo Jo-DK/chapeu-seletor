5ba0df4c495b5f7203d7afc1b65b37b787c66cce
Jonathan <jonathan.nsdk@ateliedepropaganda.com.br>
2024-12-02 01:15:42 -0300

    feat: trying souve factory

--- app/Models/Guild.php
+++ app/Models/Guild.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Guild extends Model
 {
+    /** @use HasFactory<\Database\Factories\GuildFactory> */
     use HasFactory;
     
     protected $fillable = [
